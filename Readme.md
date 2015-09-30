741cca1fd6e13f720bd222eb07ce4db6c33983ca
Ben Hutchison <ben@bluejeans.com>
2015-09-29 18:54:20 -0700

    Added Readme. Updated server to include status and (un)mutemicrophone methods.

---
+++ Readme.md
@@ -0,0 +1,31 @@
+Blue Jeans Relay Mesh Example Server in Node.js
+===============================================
+
+Mesh is an HTTP interface that allows [Blue Jeans Relay](http://bluejeans.com/features/relay) to integrate with otherwise unsupported Endpoints.
+
+The Relay Listener Service can send commands like join and hangup to this custom Mesh server, which will use custom integration logic to cause the unsupported Endpoint to carry out the command.
+
+See the [Relay API docs](https://relay.bluejeans.com/docs/mesh.html) for more details and API specifications.
+
+## Requirements
+- [Blue Jeans Relay account](http://bluejeans.com/features/relay#relay)
+- [PHP](http://php.net/downloads.php)
+- Web server with PHP support, like [Apache httpd](http://httpd.apache.org/download.cgi) or [Nginx](http://nginx.org/en/download.html)
+- [Composer](https://getcomposer.org/download/)
+
+## Installation
+    cd /var/www # or wherever your server's web root is
+    git clone https://github.com/Aldaviva/relay-mesh-example-php.git
+    cd relay-mesh-example-php
+    composer install
+
+The HTTP interface will be served from <code>/relay-mesh-example-php</code>. If <code>mod_rewrite</code> and <code>AllowOverride</code> permit it, you can send a GET request to <code>http://127.0.0.1/relay-mesh-example-php/1.2.3.4/capabilities</code>, otherwise you must use <code>/relay-mesh-example-php/index.php/1.2.3.4/capabilities</code>.
+
+Requests will be parsed and logged.
+
+## Start coding
+
+Check out the resource methods in `index.php`.
+
+From here you can implement your own logic when different requests are handled.
+
--- index.php
+++ index.php
@@ -6,17 +6,46 @@ $app = new \Slim\Slim(array(
     'log.enabled' => true
 ));
 
-$app->get('/:ipAddress/callactive', function($ipAddress) use ($app) {
-    $app->log->info("Received callactive request\n".print_r(array(
+/**
+ * Capabilities method
+ * @see https://relay.bluejeans.com/docs/mesh.html#capabilities
+ */
+$app->get('/:ipAddress/capabilities', function($ipAddress) use ($app) {
+    $app->log->info("Received capabilities request\n".print_r(array(
         'ipAddress' => $ipAddress,
         'port' => $app->request->get('port'),
         'name' => $app->request->get('name')
     ), true));
 
     $app->response->headers->set('Content-Type', 'application/json');
-    echo json_encode(array('callActive' => false));
+    echo json_encode(array(
+        'JOIN' => true,
+        'HANGUP' => true,
+        'STATUS' => true,
+        'MUTE_MICROPHONE' => true));
 });
 
+/**
+ * Status method
+ * @see https://relay.bluejeans.com/docs/mesh.html#status
+ */
+$app->get('/:ipAddress/status', function($ipAddress) use ($app) {
+    $app->log->info("Received status request\n".print_r(array(
+        'ipAddress' => $ipAddress,
+        'port' => $app->request->get('port'),
+        'name' => $app->request->get('name')
+    ), true));
+
+    $app->response->headers->set('Content-Type', 'application/json');
+    echo json_encode(array(
+            'callActive' => false,
+            'microphoneMuted' => false));
+});
+
+/**
+ * Join method
+ * @see https://relay.bluejeans.com/docs/mesh.html#join
+ */
 $app->post('/:ipAddress/join', function($ipAddress) use ($app) {
     $app->log->info("Received join request\n".print_r(array(
         'ipAddress' => $ipAddress,
@@ -30,9 +59,38 @@ $app->post('/:ipAddress/join', function($ipAddress) use ($app) {
     $app->response->setStatus(204);
 });
 
-
+/**
+ * Hangup method
+ * @see https://relay.bluejeans.com/docs/mesh.html#hangup
+ */
 $app->post('/:ipAddress/hangup', function($ipAddress) use ($app) {
     $app->log->info("Received hangup request\n".print_r(array(
+        'ipAddress' => $ipAddress,
+        'endpoint' => json_decode($app->request->getBody())
+    ), true));
+
+    $app->response->setStatus(204);
+});
+
+/**
+ * Mute Microphone method
+ * @see https://relay.bluejeans.com/docs/mesh.html#mutemicrophone
+ */
+$app->post('/:ipAddress/mutemicrophone', function($ipAddress) use ($app) {
+    $app->log->info("Received mutemicrophone request\n".print_r(array(
+        'ipAddress' => $ipAddress,
+        'endpoint' => json_decode($app->request->getBody())
+    ), true));
+
+    $app->response->setStatus(204);
+});
+
+/**
+ * Unmute Microphone method
+ * @see https://relay.bluejeans.com/docs/mesh.html#mutemicrophone
+ */
+$app->post('/:ipAddress/unmutemicrophone', function($ipAddress) use ($app) {
+    $app->log->info("Received unmutemicrophone request\n".print_r(array(
         'ipAddress' => $ipAddress,
         'endpoint' => json_decode($app->request->getBody())
     ), true));
